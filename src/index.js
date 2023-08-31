e06b79dcaee84417647ccb5c087d1f4a3b3270f3
Cater <84566931+SuperCater@users.noreply.github.com>
2023-08-31 12:52:43 -0400

    Removed config system

--- src/index.js
+++ src/index.js
@@ -17,49 +17,46 @@ const groupURL = "https://groups.roblox.com/v1/groups/"
 
 
 RankTracker.index = async (groupID, options) => {
-	let data;
-	for (const group of options.groups) {
-		if (groupID && group.id !== groupID) continue
-		try {
-			await axios.get(groupURL + group.id)
-		} catch (e) {
-			continue
-		}
+	try {
+		await axios.get(groupURL + group.id)
+	} catch (e) {
+		continue
+	}
 
-		data = {
-			groupId: group.id,
-			groupName: group.name,
-			ranksToIndex: group.ranks,
-			roles: [],
-		}
+	let data = {
+		groupId: group.id,
+		groupName: group.name,
+		ranksToIndex: group.ranks,
+		roles: [],
+	}
+	// TODO: Verify the group exists
 
-		const roles = await axios.get(groupURL + group.id + "/roles").then(res => res.data.roles)
+	const roles = await axios.get(groupURL + group.id + "/roles").then(res => res.data.roles)
 
-		for (const role of roles) {
-			if (!group.ranks.includes(role.rank)) continue
-			const members = []
-			let next;
-			const start = Date.now()
-			while (true) {
-				let link = groupURL + group.id + "/roles/" + role.id + "/users?limit=100"
-				if (next) link += "&cursor=" + next
-				const data = await get(link)
-				// const data = await axios.get(link).then(res => res.data)
-				for (const member of data.data) {
-					members.push({
-						username: member.username,
-						userId: member.userId,
-					})
-				}
-				next = data.nextPageCursor
-				if (next === null) break;
+	for (const role of roles) {
+		if (!group.ranks.includes(role.rank)) continue
+		const members = []
+		let next;
+		const start = Date.now()
+		while (true) {
+			let link = groupURL + group.id + "/roles/" + role.id + "/users?limit=100"
+			if (next) link += "&cursor=" + next
+			const data = await get(link)
+			// const data = await axios.get(link).then(res => res.data)
+			for (const member of data.data) {
+				members.push({
+					username: member.username,
+					userId: member.userId,
+				})
 			}
-			data.roles.push({
-				name: role.name,
-				rank: role.rank,
-				members,
-			})
+			next = data.nextPageCursor
+			if (next === null) break;
 		}
+		data.roles.push({
+			name: role.name,
+			rank: role.rank,
+			members,
+		})
 	}
 	return data
 }
