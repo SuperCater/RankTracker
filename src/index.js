ba3176a6b9b4aa275011d727a9efbb15a1567c25
Cater <84566931+SuperCater@users.noreply.github.com>
2023-08-31 12:51:11 -0400

    Removed diff function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import fs from 'fs'
-import path from 'path'
 
 const get = async (url) => {
 	try {
@@ -19,42 +17,26 @@ const groupURL = "https://groups.roblox.com/v1/groups/"
 
 
 RankTracker.index = async (groupID, options) => {
-	for (const group of config.groups) {
+	let data;
+	for (const group of options.groups) {
 		if (groupID && group.id !== groupID) continue
-		log("info", `Indexing group ${group.name}...`)
-		log("info", `Verifying group ${group.name} exists...`)
 		try {
 			await axios.get(groupURL + group.id)
-			log("success", `Group ${group.name} exists!`)
 		} catch (e) {
-			log("error", `Failed to get group data for group ${group.name}!`)
 			continue
 		}
 
-		log("info", `Creating file name for group ${group.name}...`)
-		const name = `${group.name}-Index.json`
-		const data = {
+		data = {
 			groupId: group.id,
 			groupName: group.name,
 			ranksToIndex: group.ranks,
 			roles: [],
 		}
-		/* fs.writeFileSync(`./data/indexes/${name}`, JSON.stringify({
-			data,
-		})) */
-		
-		log("success", `Created file name for group ${group.name}!`)
-
-
-
-		log("info", `Getting group roles for group ${group.name}...`)
 
 		const roles = await axios.get(groupURL + group.id + "/roles").then(res => res.data.roles)
-		log("success", `Got group roles for group ${group.name}!`)
 
 		for (const role of roles) {
 			if (!group.ranks.includes(role.rank)) continue
-			log("info", `Getting group members for role ${role.name}. This may take a minute...`)
 			const members = []
 			let next;
 			const start = Date.now()
@@ -72,71 +54,19 @@ RankTracker.index = async (groupID, options) => {
 				next = data.nextPageCursor
 				if (next === null) break;
 			}
-			log("success", `Got group members for role ${role.name}!`)
-			log("info", `Took ${Date.now() - start}ms to get members for role ${role.name}!`)
-			log("info", `Adding group members for role ${role.name} to data...`)
 			data.roles.push({
 				name: role.name,
 				rank: role.rank,
 				members,
 			})
-			log("success", `Added rank members to data for role ${role.name}!`)
 		}
-		fs.writeFileSync(`./data/indexes/${name}`, JSON.stringify(data))
-		log("success", `Wrote group members to file for group ${group.name}!`)
 	}
-	log("success", "Finished indexing groups!")
+	return data
 }
 
 
-RankTracker.diff = async () => {
-	for (const group of config.groups) {
-		let ogIndex;
-		log("info", `Diffing group ${group.name}...`)
-		const indexes = fs.readdirSync("./data/indexes").filter(file => file === `${group.name}-Index.json`)
-		if (indexes.length === 0) {
-			log("warn", `No index found for group ${group.name}!`)
-			index(group.id)
-			continue
-		}
-		if (indexes.length > 1) {
-			log("warn", `Multiple indexes found for group ${group.name}! Using the first one...`)
-		}
-		ogIndex = JSON.parse(fs.readFileSync(`./data/indexes/${indexes[0]}`))
-		log("info", `Got index for group ${group.name}!`)
-		log("info", `Getting new index for group ${group.name}...`)
-		await index(group.id)
-
-		const newIndex = JSON.parse(fs.readFileSync(`./data/indexes/${indexes[0]}`))
-		let diffName = `${group.name}-Diff-${new Date().toISOString().split("T")[0]}.txt`
-		const diffs = fs.readdirSync("./data/diffs").filter(file => file.startsWith(diffName))
-		if (diffs.includes(diffName)) {
-			diffName = diffName.replace(".txt", `-${diffs.length}.txt`)
-		}
-		fs.writeFileSync(`./data/diffs/${diffName}`, "Diffing group " + group.name + "...\n")
-
-		log("info", `Diffing group ${group.name}...`)
-		for (const role of ogIndex.roles) {
-			const newRole = newIndex.roles.find(r => r.name === role.name)
-			for (const member of role.members) {
-				const newMember = newRole.members.find(m => m.userId === member.userId)
-
-
-				if (!newMember) {
-					log("info", `Member ${member.username} is no longer in role ${role.name}!`)
-					// Find their new role
-					const newRole = newIndex.roles.find(r => r.members.find(m => m.userId === member.userId))
-					if (!newRole) {
-						log("info", `Member ${member.username} is no longer in the group! Was in role ${role.name}!`)
-						fs.appendFileSync(`./data/diffs/${diffName}`, `$${member.username}(${member.userId}) is no longer in the group! Was a ${role.name}:\n`)
-						continue
-					}
-					log("info", `Member ${member.username} is now in role ${newRole.name}!`)
-					fs.appendFileSync(`./data/diffs/${diffName}`, `${member.username}(${member.userId}) ${role.name}: => ${newRole.name}\n`)
-				}
-			}
-		}
-	}
+RankTracker.diff = async (first, second, options) => {
+	//TODO: Rewrite all of this
 }
 
 
